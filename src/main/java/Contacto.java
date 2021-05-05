614c387caa4fe5f2a60de06f40b2d7eb8be23fc0
rNNdom <l.galeano01@ufromail.cl>
2021-05-05 09:23:11 -0400

    1era versión con setters & getters

---
+++ src/main/java/Contacto.java
@@ -0,0 +1,45 @@
+public class Contacto {
+    private String nombre;
+    private String apellido;
+    private String numero;
+    private String direccion;
+
+    public Contacto(String nombre, String apellido, String numero, String direccion) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.numero = numero;
+        this.direccion = direccion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+}
